010288bc018d39aa5c62ad8edfdef398d842ad94
Vatsal <vatsalr23@gmail.com>
2020-11-09 19:23:55 -0800

    First commit

---
+++ src/main/kotlin/com/vatsal/spotifyspringclient/SpotifySpringClientApplication.kt
@@ -0,0 +1,11 @@
+package com.vatsal.spotifyspringclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifySpringClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotifySpringClientApplication>(*args)
+}
---
+++ src/test/kotlin/com/vatsal/spotifyspringclient/SpotifySpringClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vatsal.spotifyspringclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotifySpringClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
