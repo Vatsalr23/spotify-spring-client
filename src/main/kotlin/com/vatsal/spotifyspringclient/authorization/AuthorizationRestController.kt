786f0837ff0c71b31b8f77ee7750309ab02f72e2
Vatsal <vatsalr23@gmail.com>
2020-11-11 10:37:58 -0800

    feature/added refresh token endpoint and temporary token saving mechanism

--- src/main/kotlin/com/vatsal/spotifyspringclient/authorization/AuthorizationRestController.kt
+++ src/main/kotlin/com/vatsal/spotifyspringclient/authorization/AuthorizationRestController.kt
@@ -1,6 +1,6 @@
 package com.vatsal.spotifyspringclient.authorization
 
-import com.vatsal.spotifyspringclient.integration.spotify.SpotifyAuthApiClient
+import com.vatsal.spotifyspringclient.integration.spotify.AuthorizationApiClient
 import com.vatsal.spotifyspringclient.integration.spotify.TokenResponse
 import mu.KLogging
 import org.springframework.beans.factory.annotation.Value
@@ -9,12 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.view.RedirectView
-import java.util.*
 
 @RestController
 @RequestMapping("/v1")
 class AuthorizationRestController(
-    private val spotifyAuthApiClient: SpotifyAuthApiClient,
+    private val authorizationApiClient: AuthorizationApiClient,
     @Value("\${spotify.client-id}") private val clientId: String,
     @Value("\${spotify.client-secret}") private val clientSecret: String,
     @Value("\${spotify.authorization.redirect-uri}") private val redirectURI: String,
@@ -41,5 +40,11 @@ class AuthorizationRestController(
     fun login(): RedirectView {
         logger.info { "Received request to login." }
         return RedirectView(authorizationService.getSpotifyRedirectURL())
+    }
+
+    @GetMapping("/refreshTokens")
+    fun refreshAccessToken(): TokenResponse {
+        logger.info { "Refresh token request received. Refreshing access token." }
+        return authorizationService.refreshAccessToken()
     }
 }
--- src/main/kotlin/com/vatsal/spotifyspringclient/integration/spotify/TokenResponse.kt
+++ src/main/kotlin/com/vatsal/spotifyspringclient/integration/spotify/TokenResponse.kt
@@ -1,11 +1,18 @@
 package com.vatsal.spotifyspringclient.integration.spotify
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class TokenResponse(
-	@JsonProperty("access_token") val accessToken: String,
-	@JsonProperty("token_type") val tokenType: String,
-	@JsonProperty("scope") val scope: String,
-	@JsonProperty("expires_in") val expiresIn: Long,
-	@JsonProperty("refresh_token") val refreshToken: String
+	@JsonProperty("access_token")
+	val accessToken: String,
+	@JsonProperty("token_type")
+	val tokenType: String,
+	@JsonProperty("scope")
+	val scope: String,
+	@JsonProperty("expires_in")
+	val expiresIn: Long,
+	@JsonProperty("refresh_token")
+	val refreshToken: String?
 )
